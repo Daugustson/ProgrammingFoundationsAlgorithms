92d98db03a6616f64bdd18470d8ae3d8929266a5
Dayn Augustson <42788758+Daugustson@users.noreply.github.com>
2021-02-23 15:52:17 -0700

    Stack and Queue
    added new learning code for Stack and Queue

--- Exercise Files/2 Data Structures/Queues/queue_start.py
+++ Exercise Files/2 Data Structures/Queues/queue_start.py
@@ -1,13 +1,20 @@
 # try out the Python queue functions
+from collections import deque
 
 
 # TODO: create a new empty deque object that will function as a queue
-
+queue = deque()
 
 # TODO: add some items to the queue
-
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
 
 # TODO: print the queue contents
-
+print(queue)
 
 # TODO: pop an item off the front of the queue
+x = queue.popleft
+print(x)
+print(queue) 
