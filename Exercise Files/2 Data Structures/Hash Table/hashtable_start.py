4cdab4a0ce1d0a69d6ccee572556ed4eeda88b07
Dayn Augustson <42788758+Daugustson@users.noreply.github.com>
2021-02-23 17:04:14 -0700

    Update hashtable_start.py
    Playing with hashtable

--- Exercise Files/2 Data Structures/Hash Table/hashtable_start.py
+++ Exercise Files/2 Data Structures/Hash Table/hashtable_start.py
@@ -2,13 +2,18 @@
 
 
 # TODO: create a hashtable all at once
-
+items1 = dict({"key1": 1, "key2": 2, "key3": "three"})
+print(items1)
 
 # TODO: create a hashtable progressively
-
+items2 = {}
+items2["key1"] = 1
+items2["key2"] = 2
+items2["key3"] = 3
+print(items2)
 
 # TODO: try to access a nonexistent key
-
+print(items1["key6"])
 
 # TODO: replace an item
 
