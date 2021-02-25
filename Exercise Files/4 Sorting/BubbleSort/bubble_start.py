863d58a29b7c8d2af825dc34a3f793dc789653ae
Dayn Augustson <42788758+Daugustson@users.noreply.github.com>
2021-02-25 16:24:41 -0700

    Update bubble_start.py
    Bubble sorting

--- Exercise Files/4 Sorting/BubbleSort/bubble_start.py
+++ Exercise Files/4 Sorting/BubbleSort/bubble_start.py
@@ -3,7 +3,15 @@
 
 def bubbleSort(dataset):
     # TODO: start with the array length and decrement each time
-
+    if i in range(len(dataset) -1, 0, -1):
+         # examine each item pair a loop inside a loop is uaslly Big O n Squared 
+        for j in range(i):
+            # swap items if needed
+            if dataset[j] > dataset[j+1]:
+                temp = dataset[j]
+                dataset[j] = dataset[j+1]
+                dataset[j+1] = temp
+    
     print("Current state: ", dataset)
 
 
